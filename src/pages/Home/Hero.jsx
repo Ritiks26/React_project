cd8c5b10beb4e08f6c3365219ede2e94099e2be8
Ritik <teragmail@example.com>
2025-10-08 16:32:23 +0530

    banner

--- src/pages/Home/Hero.jsx
+++ src/pages/Home/Hero.jsx
@@ -1,20 +1,45 @@
+import gsap from "gsap";
+import { useEffect, useRef } from "react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import "./Hero.css";
 
+// gsap.registerPlugin(ScrollTrigger);
+
 export function Hero() {
+  // const animateRef = useRef(null);
+  // const triggerRef = useRef(null);
+
+  // useEffect(() => {
+  //   gsap.to(animateRef.current, {
+  //     width: "30vw",
+  //     scrollTrigger: {
+  //       trigger: triggerRef.current,
+  //       start: "top top",
+  //       end: "top 20%",
+  //       scrub: true,
+  //       markers: true,
+  //     },
+  //   });
+  // }, []);
+
   return (
-    <div className="hero-banner">
-      <div className="hero-pic">
-        <img
-          src="https://akkeknitwear.com/website/wp-content/uploads/2023/10/akke-3-1.webp"
-          alt=""
-        />
+    <>
+      <div className="hero-banner">
+        <div className="hero-pic">
+          <img
+            className="ad-image"
+            src="https://akkeknitwear.com/website/wp-content/uploads/2023/11/LIMITED.webp"
+            alt=""
+          />
+          <p className="ad-text">FEEL THE DIFFERENCE</p>
+        </div>
+        <div className="hero-text">
+          <div className="text">
+            RSNB IS THE NEW NORM
+            <div className="width-increasing"></div>
+          </div>
+        </div>
       </div>
-      <div className="hero-pic">
-        <img
-          src="https://akkeknitwear.com/website/wp-content/uploads/2023/10/akke-3-1.webp"
-          alt=""
-        />
-      </div>
-    </div>
+    </>
   );
 }
