78083af260dd1df3e66f26f7146a37cb4a05d0d5
Ritik <teragmail@example.com>
2025-10-19 16:52:00 +0530

    finalizing project

--- src/pages/Check-product/ProductInfoAccordion.jsx
+++ src/pages/Check-product/ProductInfoAccordion.jsx
@@ -9,43 +9,48 @@ const sections = [
     content:
       "International orders will be delivered express through DHL Express. All orders will be sent with a tracking number once dispatched from the warehouse. For more information about shipping, see here.",
   },
+  {
+    title: "Fabric Information",
+    content:
+      "International orders will be delivered express through DHL Express. All orders will be sent with a tracking number once dispatched from the warehouse. For more information about shipping, see here. International orders will be delivered express through DHL Express. All orders will be sent with a tracking number once dispatched from the warehouse. For more information about shipping, see here. International orders will be delivered express through DHL Express. All orders will be sent with a tracking number once dispatched from the warehouse. For more information about shipping, see here",
+  },
 ];
 
 export function ProductInfoAccordion() {
-  const [sectionOpen, setSectionOpen] = useState(false);
-  const iconRef = useRef(null);
-  const sectionRef = useRef(null);
-  const textRef = useRef(null);
-  const toggleSection = () => {
-    if (!sectionOpen) {
-      setSectionOpen(true);
+  const [sectionOpen, setSectionOpen] = useState(null);
+  const iconRef = useRef([]);
+  const sectionRef = useRef([]);
+  const textRef = useRef([]);
+  const toggleSection = (i) => {
+    const isOpen = sectionOpen === i;
+    setSectionOpen(isOpen ? null : i);
 
-      gsap.to(iconRef.current, {
+    if (!isOpen) {
+      gsap.to(iconRef.current[i], {
         rotate: 45,
         duration: 0.3,
       });
       gsap.fromTo(
-        textRef.current,
+        textRef.current[i],
         { opacity: 0, y: -15 },
         { opacity: 1, y: 0, duration: 0.4, delay: 0.2, ease: "power2.out" }
       );
-      gsap.to(sectionRef.current, {
+      gsap.to(sectionRef.current[i], {
         height: "28vh",
         duration: 0.6,
       });
     } else {
-      gsap.to(iconRef.current, {
+      gsap.to(iconRef.current[i], {
         rotate: 0,
         duration: 0.3,
       });
-      gsap.to(textRef.current, {
+      gsap.to(textRef.current[i], {
         opacity: 0,
         y: -15,
         duration: 0.3,
         ease: "power2.in",
-        onComplete: () => setSectionOpen(false),
       });
-      gsap.to(sectionRef.current, {
+      gsap.to(sectionRef.current[i], {
         height: "5vh",
         duration: 0.6,
       });
@@ -53,14 +58,31 @@ export function ProductInfoAccordion() {
   };
   return (
     <div className="helping-container">
-      {sections.map((sec) => (
-        <div ref={sectionRef} className="outer-container">
+      {sections.map((sec, i) => (
+        <div
+          key={i}
+          ref={(el) => (sectionRef.current[i] = el)}
+          className="outer-container"
+        >
           <div className="helping-container-child">
-            <p onClick={toggleSection}>{sec.title}</p>
-            <img ref={iconRef} onClick={toggleSection} src={plusIcon} alt="" />
+            <p
+              onClick={() => {
+                toggleSection(i);
+              }}
+            >
+              {sec.title}
+            </p>
+            <img
+              ref={(el) => (iconRef.current[i] = el)}
+              onClick={() => {
+                toggleSection(i);
+              }}
+              src={plusIcon}
+              alt=""
+            />
           </div>
           <div className="content">
-            <p ref={textRef}>
+            <p ref={(el) => (textRef.current[i] = el)}>
               <strong></strong>
               {sec.content}
             </p>
--- src/pages/Check-product/SuggestedProducts.jsx
+++ src/pages/Check-product/SuggestedProducts.jsx
@@ -7,28 +7,27 @@ export function SuggestedProducts({ allProducts }) {
     <div className="suggested-product-all">
       <h1>YOU MAY ALSO LIKE_</h1>
       <div className="suggested-product-parent">
-        {allProducts.slice(9, 13).map((product) => (
+        {allProducts.slice(0, 4).map((product) => (
           <div key={product.id} className="suggested-product-container">
-            <div className="suggested-image-wrapper">
-              <img
-                className="suggested-product-image"
-                src={
-                  product.colors
-                    ? product.colors[2].image
-                    : "something went wrong"
-                }
-              />
-              <Link to={`/product/${product.id}`}>
-                <button className="view-product-button">VIEW PRODUCT</button>
-              </Link>
-            </div>
+            <Link to={`/product/${product.id}`}>
+              <div className="suggested-image-wrapper">
+                <img
+                  className="suggested-product-image"
+                  src={
+                    product.colors
+                      ? product.colors[2].image
+                      : "something went wrong"
+                  }
+                />
+              </div>
+            </Link>
 
             <div className="suggested-product-content">
               <p className="suggested-product-name">{product.name}</p>
               <p className="suggested-product-color">
                 <strong>
                   {product.colors
-                    ? product.colors[2].nameForRendering.toUpperCase()
+                    ? product.colors[2].name.toUpperCase()
                     : product.name}
                 </strong>
               </p>
